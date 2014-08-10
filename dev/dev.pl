7a88529b31f28213305db9f95201832dacb827eb
Leon Wright <techman83@gmail.com>
2014-08-10 15:12:45 +0800

    dev stuffs

--- dev/dev.pl
+++ dev/dev.pl
@@ -9,7 +9,7 @@ $ENV{STRAVA_DEGUG} = 1;
 
 my $strava = WebService::Strava->new();
 
-my $athlete = $strava->athlete("226646");
+#my $athlete = $strava->athlete();
 
 #my $segment = $strava->auth->get("https://www.strava.com/api/v3/segments/3468536");
 
@@ -20,4 +20,8 @@ my $athlete = $strava->athlete("226646");
 #print Dumper($segment);
 #my $efforts = $segment->list_efforts(athlete_id => 226646);
 
-print Dumper($athlete);
+#@{$athlete->{clubs}}[0]->retrieve();
+
+my $effort = $strava->effort("4101511026");
+
+print Dumper($effort);
